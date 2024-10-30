6f2a17ffbe23bf0261e079ea669d9bbfc76ff9bf
Cristian Eduardo <cristianeduardoph@gmail.com>
2024-10-30 21:45:50 +0000

    Add vista de carga y consulta a la API con JS

---
+++ js/app.js
@@ -0,0 +1,24 @@
+async function fetchWithRetry(url, options, retyDely = 5000) {
+    while (true) {
+        try {
+            const response = await fetch(url, options)
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}: ${response.statusText}`)
+            }
+            // Si la respuesta es exitosa, la funcion continua
+            return await response.json()
+        } catch (error) {
+            console.log(`API no disponible. Reintentando en ${retryDelay / 1000} segundos...`);
+            await new Promise(resolve => setTimeout(resolve, retryDelay));
+        }
+    }
+}
+
+// Uso de la función
+(async () => {
+    const data = await fetchWithRetry('https://node-api-blackout.onrender.com/horario');
+    console.log("Datos recibidos:", data);
+    // Aquí continua el funcionamiento normal de la app.
+    window.location.href = "../home.html";
+    // window.open("../index.html", "_blank");
+})();
